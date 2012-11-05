2f41fe517d614850bae8be7298e8d8aa4d318513
Greg Hutchinson <the.greg.hutchinson@gmail.com>
2012-11-05 09:30:54 -0600

    Simplified Classes

--- src/test/java/ca/attractors/dot/Edge.java
+++ src/test/java/ca/attractors/dot/Edge.java
@@ -3,22 +3,53 @@ package ca.attractors.dot;
 public class Edge extends DotType {
 	private String from;
 	private String to;
-	private EdgeAttributes attributes;
 	private GraphType graphType;
 
 	public Edge(String aFrom, String aTo, GraphType aGraphType) {
 		from = aFrom;
 		to = aTo;
-		attributes = new EdgeAttributes();
 		graphType = aGraphType;
 	}
+	private static final String LABEL = "label";
+	private static final String FILLCOLOR = "fillcolor";
+	private static final String COLOR = "color";
+	private static final String STYLE = "style";
 
-	public EdgeAttributes getAttributes() {
-		return attributes;
+	public String getLabel() {
+		return get(LABEL);
 	}
 
-	public String toDotString() {
-		return from + " " + getEdgeString() + " " + to + " " + attributes.toDotString() + "\n";
+	public void setLabel(String aLabel) {
+		set(LABEL, aLabel);
+	}
+
+	public String getFillColor() {
+		return get(FILLCOLOR);
+	}
+
+	public void setFillColor(String aFillColor) {
+		set(FILLCOLOR, aFillColor);
+	}
+
+	public String getStyle() {
+		return get(STYLE);
+	}
+
+	public void setStyle(String aStyle) {
+		set(STYLE, aStyle);
+	}
+
+	public void setColor(String aString) {
+		set(COLOR, aString);
+	}
+
+	public String getColor() {
+		return get(COLOR);
+	}
+
+
+	public String getDefinitionString() {
+		return from + " " + getEdgeString() + " " + to;
 	}
 
 	private String getEdgeString() {
