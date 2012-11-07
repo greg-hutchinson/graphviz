b33ce02d64542d83a8d4d4caf6162ed7262064c2
Greg Hutchinson <the.greg.hutchinson@gmail.com>
2012-11-06 21:01:56 -0600

    With small test cases for the last commit

--- src/test/java/ca/attractors/dot/GraphTest.java
+++ src/test/java/ca/attractors/dot/GraphTest.java
@@ -16,6 +16,8 @@ public class GraphTest extends TestCase {
 //		node.setFontname("abc");
 		graph.setBackgroundColor("red");
 		Edge edge = graph.newEdge("a", "b");
+		EdgeAttributes edgeAttributes = graph.newEdgeAttributes();
+		edgeAttributes.setColor("blue");
 		Subgraph subgraph = graph.newSubgraph("sub1");
 		subgraph.newNode("d");
 		assertEquals(getExpectedComplexString(), graph.toDotString());
@@ -27,6 +29,7 @@ public class GraphTest extends TestCase {
 		builder.append("graph [bgcolor=\"red\"]\n");
 		builder.append("\"node1\"\n");
 		builder.append("\"a\" -> \"b\"\n");
+		builder.append("Edge [color=\"blue\"]\n");
 		builder.append("subgraph \"sub1\" {\n");
 		builder.append("}\n}\n");
 		return builder.toString();
