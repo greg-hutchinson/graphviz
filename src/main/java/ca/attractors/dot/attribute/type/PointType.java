0a3a9099ec12d6030e364f4d6b9fae5d0aad371b
Ryan Flegel <rflegel@gmail.com>
2012-11-06 21:52:49 -0600

    Attribute types

---
+++ src/main/java/ca/attractors/dot/attribute/type/PointType.java
@@ -0,0 +1,36 @@
+package ca.attractors.dot.attribute.type;
+
+public final class PointType implements IDotAttributeValue {
+	
+	private final double x;
+	private final double y;
+	private final Double z;
+	private final boolean nodePositionShouldNotChange;
+	
+	public PointType(double anX, double aY) {
+		this(anX, aY, false);
+	}
+	
+	public PointType(double anX, double aY, double aZ) {
+		this(anX, aY, aZ, false);
+	}
+	
+	public PointType(double anX, double aY, boolean aNodePositionShouldNotChange) {
+		x = anX;
+		y = aY;
+		z = null;
+		nodePositionShouldNotChange = aNodePositionShouldNotChange;
+	}
+	
+	public PointType(double anX, double aY, double aZ, boolean aNodePositionShouldNotChange) {
+		x = anX;
+		y = aY;
+		z = aZ;
+		nodePositionShouldNotChange = aNodePositionShouldNotChange;
+	}
+	
+	public String getValue() {
+		return String.valueOf(x) + "," + String.valueOf(y) + (z != null ? "," + z : "") + (nodePositionShouldNotChange ? "!" : "");
+	}
+
+}
