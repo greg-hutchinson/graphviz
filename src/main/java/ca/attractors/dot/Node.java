8966815a2b769079615697b413cbd9a919d5d98e
hutching <hutching@localhost>
2013-04-02 09:12:48 -0600

    Draft 6

--- src/main/java/ca/attractors/dot/Node.java
+++ src/main/java/ca/attractors/dot/Node.java
@@ -18,30 +18,6 @@ public class Node extends DotObject implements IGraphElement {
 		return name;
 	}
 
-	public String getLabel() {
-		return get(LABEL);
-	}
-
-	public void setLabel(String aLabel) {
-		set(LABEL, aLabel);
-	}
-
-	public String getFillColor() {
-		return get(FILLCOLOR);
-	}
-
-	public void setFillColor(String aFillColor) {
-		set(FILLCOLOR, aFillColor);
-	}
-
-	public String getStyle() {
-		return get(STYLE);
-	}
-
-	public void setStyle(String aStyle) {
-		set(STYLE, aStyle);
-	}
-
 
 	@Override
 	public boolean equals(Object aObj) {
