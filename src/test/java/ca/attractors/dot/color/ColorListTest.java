529dd1899ca3865c19aa7462441d26dbe8c488d2
Greg Hutchinson <the.greg.hutchinson@gmail.com>
2013-04-04 14:29:32 -0600

    Test Cases Working - toDotString and getValue well defined

--- src/test/java/ca/attractors/dot/color/ColorListTest.java
+++ src/test/java/ca/attractors/dot/color/ColorListTest.java
@@ -4,18 +4,27 @@ import junit.framework.TestCase;
 
 public class ColorListTest extends TestCase {
 
-	public void testGetValue() {
-		ColorList list1 = new ColorList();
-		ColorList list2 = new ColorList();
-		WeightedColor color = new WeightedColor(X11NamedColor.ALICEBLUE, (float) 0.21);
-		list1.addColor(color);
-		assertEquals("aliceblue;0.21", list1.getValue());
-		assertEquals("\"aliceblue;0.21\"", list1.toDotString());
+	private ColorList list1;
+	private ColorList list2;
+	private WeightedColor color;
+
+	@Override
+	protected void setUp() throws Exception {
+		list1 = new ColorList();
+		list2 = new ColorList();
+		color = new WeightedColor(X11NamedColor.ALICEBLUE, (float) 0.21);
 		list2.addColor(color);
+		list1.addColor(color);
 		list1.addColor(list2);
+	}
+	public void testGetValue() {
+		assertEquals("aliceblue;0.21", list2.getValue());
 		assertEquals("aliceblue;0.21:aliceblue;0.21", list1.getValue());
+	}
+
+	public void testToDotString() {
+		assertEquals("\"aliceblue;0.21\"", list2.toDotString());
 		assertEquals("\"aliceblue;0.21:aliceblue;0.21\"", list1.toDotString());
-		System.out.println(list1.getValue());
 	}
 
 }
