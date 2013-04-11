32b41451ffa29d87d17b7640baf4b55b22cda062
Adam Ramadan <adam.ramadan@gmail.com>
2013-04-11 14:43:11 -0600

    refactored to removed duplication

--- src/main/java/ca/attractors/dot/html/HtmlTable.java
+++ src/main/java/ca/attractors/dot/html/HtmlTable.java
@@ -4,51 +4,9 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import ca.attractors.dot.color.IColor;
-
-public class HtmlTable extends HtmlTableBase {
+public class HtmlTable extends HtmlTableBase<HtmlTable> {
 
 	private List<HtmlTableRow> rows = new ArrayList<HtmlTableRow>();
-
-	public HtmlTable setAlign(Alignment anAlign) {
-		this.align = anAlign;
-		return this;
-	}
-
-	public HtmlTable setBgcolor(IColor aColor) {
-		bgcolor = aColor;
-		return this;
-	}
-
-	public HtmlTable setBorder(int aBorder) {
-		border = aBorder;
-		return this;
-	}
-
-	public HtmlTable setCellborder(int aCellBorder) {
-		cellborder = aCellBorder;
-		return this;
-	}
-
-	public HtmlTable setCellpadding(int aCellPadding) {
-		cellpadding = aCellPadding;
-		return this;
-	}
-
-	public HtmlTable setCellspacing(int aCellSpacing) {
-		cellspacing = aCellSpacing;
-		return this;
-	}
-
-	public HtmlTable setColor(IColor aColor) {
-		color = aColor;
-		return this;
-	}
-
-	public HtmlTable setFixedsize(boolean aFixedSize) {
-		fixedsize = aFixedSize;
-		return this;
-	}
 
 	public HtmlTableRow newRow() {
 		HtmlTableRow row = new HtmlTableRow(this);
