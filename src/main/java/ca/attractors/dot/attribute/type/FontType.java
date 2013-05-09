8c239dee605a5ad343ce4a3f83bec000522003f5
Adam Ramadan <adam.ramadan@gmail.com>
2013-05-09 09:46:50 -0600

    create enum for fonts

---
+++ src/main/java/ca/attractors/dot/attribute/type/FontType.java
@@ -0,0 +1,45 @@
+package ca.attractors.dot.attribute.type;
+
+import ca.attractors.util.Strings;
+
+public enum FontType implements IDotAttributeValue {
+	Antiqua("Antiqua"),
+	Arial("Arial"),
+	Avqest("Avqest"),
+	Blackletter("Blackletter"),
+	Calibri("Calibri"),
+	ComicSans("Comic Sans"),
+	Courier("Courier"),
+	Decorative("Decorative"),
+	Fraktur("Fraktur"),
+	Frosty("Frosty"),
+	Garamond("Garamond"),
+	Georgia("Georgia"),
+	Helvetica("Helvetica"),
+	Impact("Impact"),
+	Minion("Minion"),
+	Modern("Modern"),
+	Monospace("Monospace"),
+	Palatino("Palatino"),
+	Roman("Roman"),
+	Script("Script"),
+	Swiss("Swiss"),
+	TimesNewRoman("Times New Roman"),
+	Verdana("Verdana"),
+	NONE("none");
+	
+	private final String value;
+	
+	private FontType(String aValue) {
+		value = aValue;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+	public String toDotString() {
+		return Strings.quoted(getValue());
+	}
+
+}
