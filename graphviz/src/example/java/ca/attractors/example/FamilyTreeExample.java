25ab40c2128cc9672a069d25ed0b193a083fd55a
Greg <Greg@localhost>
2014-01-05 18:57:35 -0600

    ccommit

--- graphviz/src/example/java/ca/attractors/example/FamilyTreeExample.java
+++ graphviz/src/example/java/ca/attractors/example/FamilyTreeExample.java
@@ -1,10 +1,12 @@
 package ca.attractors.example;
 
+import ca.attractors.dot.DefaultNodeAttributes;
 import ca.attractors.dot.Edge;
 import ca.attractors.dot.Graph;
 import ca.attractors.dot.GraphType;
 import ca.attractors.dot.Node;
 import ca.attractors.dot.attribute.type.DirType;
+import ca.attractors.dot.attribute.type.FontType;
 import ca.attractors.dot.attribute.type.NodeStyleType;
 import ca.attractors.dot.color.X11NamedColor;
 import ca.attractors.dot.program.Dot;
@@ -15,11 +17,16 @@ public class FamilyTreeExample {
 
 	public static void main(String[] args) {
 		Graph graph = new Graph("FamilyTree", GraphType.DIGRAPH);
+		DefaultNodeAttributes defaultNodeAttributes = graph.newDefaultNodeAttributes();
+		defaultNodeAttributes.setFontname(FontType.TimesNewRoman);
+		defaultNodeAttributes.setFontsize("15");
 		Node greg = graph.newNode("Greg");
 		greg.setUrl("https://plus.google.com/102689275145660064358/posts");
 		Node joan = graph.newNode("Joan");
 		Node alex = graph.newNode("Alex");
 		Node steph = graph.newNode("Stephanie");
+		steph.setFontname(FontType.ComicSans);
+		steph.setFontsize("6");
 
 		alex.setFillColor(X11NamedColor.BLUE);
 		alex.setStyle(NodeStyleType.Filled);
